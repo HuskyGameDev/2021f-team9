770bb59b1def0e2363c820639b4b2318294cf22e
Brandon Hosang <brandonhosang@yahoo.com>
2021-10-18 21:59:18 -0400

    finally got the new version of procedural generation to work.

--- Delivery to Another World/Assets/Scripts/DoorCode.cs
+++ Delivery to Another World/Assets/Scripts/DoorCode.cs
@@ -6,23 +6,39 @@ public class DoorCode : MonoBehaviour
 {
     public string direction;
 
+    private void Start()
+    {
+        StartCoroutine(LockDoors());
+    }
+
+    // prevents player from leaving as soon as they enter the room
+    private IEnumerator LockDoors()
+    {
+        yield return new WaitForSeconds(5f);
+        GetComponent<BoxCollider>().enabled = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (direction == "NORTH")
         {
             FindObjectOfType<ProceduralGeneration>().moveNorth();
+            Destroy(transform.parent.gameObject);
         }
-        if (direction == "EAST")
+        else if (direction == "EAST")
         {
             FindObjectOfType<ProceduralGeneration>().moveEast();
+            Destroy(transform.parent.gameObject);
         }
-        if (direction == "SOUTH")
+        else if (direction == "SOUTH")
         {
             FindObjectOfType<ProceduralGeneration>().moveSouth();
+            Destroy(transform.parent.gameObject);
         }
-        if (direction == "WEST")
+        else if (direction == "WEST")
         {
             FindObjectOfType<ProceduralGeneration>().moveWest();
+            Destroy(transform.parent.gameObject);
         }
 
     }
--- Delivery to Another World/Assets/Scripts/ProceduralGeneration.cs
+++ Delivery to Another World/Assets/Scripts/ProceduralGeneration.cs
@@ -7,11 +7,11 @@ public class ProceduralGeneration : MonoBehaviour
 
     public int[] roomCoordinate;
     public int numberOfDifferentRooms;
+    public GameObject player;
 
     public GameObject[] prefabs;
     public int difficulty;
     private int spawn;
-    private float position;
     private int prefabAlgorithm;
     private GameObject currentPrefab;
 
@@ -19,57 +19,118 @@ public class ProceduralGeneration : MonoBehaviour
     void Start()
     {
         spawn = difficulty;
-        position = transform.position.x;
+        roomCoordinate = new int[2];
         roomCoordinate[0] = Random.Range(0, numberOfDifferentRooms);
         roomCoordinate[1] = Random.Range(0, numberOfDifferentRooms);
         prefabAlgorithm = 0;
+        spawnPrefab(algorithm(roomCoordinate));
     }
 
     // Update is called once per frame
     void Update()
     {
-        while(spawn > 0)
-        {
-            
-            spawn--;
-        }
-        FindObjectOfType<Rigidbody>().useGravity = true;
+
     }
 
-    void spawnRandomPrefab(int index)
+    // Swans in a specific prefab
+    // input: index = which prefab to spawn in
+    GameObject spawnPrefab(int index)
     {
+        FindObjectOfType<Rigidbody>().useGravity = false;
         currentPrefab = prefabs[index];
-        Vector3 trans = new Vector3(position, transform.position.y, transform.position.z);
-        Instantiate(currentPrefab, trans, transform.rotation);
-        position += 20f;
+        GameObject newRoom = Instantiate(currentPrefab, transform.position, transform.rotation);
+        FindObjectOfType<Rigidbody>().useGravity = true;
+        return newRoom;
     }
 
-    void algorithm()
+    // Does the math for determining which prefab to spawn in
+    // input: newCoords = the coordinates of the room you want to spawn in
+    // returns: the index of the prefab to be spawned in
+    int algorithm(int[] newCoords)
     {
         //do math
+        int answer = newCoords[0] ^ newCoords[1];
+        answer %= numberOfDifferentRooms;
+        return answer;
     }
 
+    // activates when player moves through north door
     public void moveNorth()
     {
         roomCoordinate[1] += 1;
-        spawnRandomPrefab(prefabAlgorithm);
+        prefabAlgorithm = algorithm(roomCoordinate);
+        GameObject newRoom = spawnPrefab(prefabAlgorithm);
+        Transform[] doors = newRoom.GetComponentsInChildren<Transform>();
+        player.GetComponent<CharacterController>().enabled = false;
+        for (int i = 0; i < doors.Length; i++)
+        {
+            if (doors[i].gameObject.CompareTag("SouthDoor"))
+            {
+                Transform door = doors[i];
+                player.GetComponent<Transform>().transform.position = door.position;
+                break;
+            }
+        }
+        player.GetComponent<CharacterController>().enabled = true;
     }
 
+    // activates when player moves through east door
     public void moveEast()
     {
         roomCoordinate[0] += 1;
-        spawnRandomPrefab(prefabAlgorithm);
+        prefabAlgorithm = algorithm(roomCoordinate);
+        GameObject newRoom = spawnPrefab(prefabAlgorithm);
+        Transform[] doors = newRoom.GetComponentsInChildren<Transform>();
+        player.GetComponent<CharacterController>().enabled = false;
+        for (int i = 0; i < doors.Length; i++)
+        {
+            if (doors[i].gameObject.CompareTag("WestDoor"))
+            {
+                Transform door = doors[i];
+                player.GetComponent<Transform>().transform.position = door.position;
+                break;
+            }
+        }
+        player.GetComponent<CharacterController>().enabled = true;
     }
 
+    // activates when player moves through south door
     public void moveSouth()
     {
         roomCoordinate[1] -= 1;
-        spawnRandomPrefab(prefabAlgorithm);
+        prefabAlgorithm = algorithm(roomCoordinate);
+        GameObject newRoom = spawnPrefab(prefabAlgorithm);
+        Transform[] doors = newRoom.GetComponentsInChildren<Transform>();
+        player.GetComponent<CharacterController>().enabled = false;
+        for (int i = 0; i < doors.Length; i++)
+        {
+            if (doors[i].gameObject.CompareTag("NorthDoor"))
+            {
+                Transform door = doors[i];
+                player.GetComponent<Transform>().transform.position = door.position;
+                break;
+            }
+        }
+        player.GetComponent<CharacterController>().enabled = true;
     }
 
+    // activates when player moves through west door
     public void moveWest()
     {
         roomCoordinate[0] -= 1;
-        spawnRandomPrefab(prefabAlgorithm);
+        prefabAlgorithm = algorithm(roomCoordinate);
+        GameObject newRoom = spawnPrefab(prefabAlgorithm);
+        Transform[] doors = newRoom.GetComponentsInChildren<Transform>();
+        player.GetComponent<CharacterController>().enabled = false;
+        for (int i = 0; i < doors.Length; i++)
+        {
+            if (doors[i].gameObject.CompareTag("EastDoor"))
+            {
+                Transform door = doors[i];
+                player.GetComponent<Transform>().transform.position = door.position;
+                break;
+            }
+        }
+        player.GetComponent<CharacterController>().enabled = true;
     }
 }
