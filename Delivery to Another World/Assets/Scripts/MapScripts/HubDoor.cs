8d3ee8bc8d0065cc46d7ff1d3a8b629d00928b44
Brandon Hosang <brandonhosang@yahoo.com>
2021-11-15 13:08:12 -0500

    added my own artwork... sorry ethan, i touched the hub 0.o

--- Delivery to Another World/Assets/Scripts/MapScripts/HubDoor.cs
+++ Delivery to Another World/Assets/Scripts/MapScripts/HubDoor.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HubDoor : MonoBehaviour
 {
     GameObject map;
+    public GameObject pressE;
     private void Start()
     {
         map = GameObject.FindGameObjectWithTag("Map");
@@ -12,8 +14,9 @@ public class HubDoor : MonoBehaviour
     }
     void Update()
     {
-        if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 3.0f)
+        if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 1.0f)
         {
+            pressE.GetComponent<Image>().color = new Vector4(255f, 255f, 255f, 255f);
             if (!map.activeSelf && Input.GetKeyDown(KeyCode.E))
             {
                 map.SetActive(true);
@@ -23,6 +26,10 @@ public class HubDoor : MonoBehaviour
             {
                 map.SetActive(false);
             }
+        }
+        else
+        {
+            pressE.GetComponent<Image>().color = new Vector4(255f, 255f, 255f, 0f);
         }
     }
 
