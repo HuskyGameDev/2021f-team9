f800828d1b6a4c0bcfb7a0c038e4af7323d4fb6a
Ethan Wright <ethanw@mtu.edu>
2021-12-01 22:17:02 -0500

    "Tutorial" added

---
+++ Delivery to Another World/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+    private bool ranTutorial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ranTutorial = false;
+    }
+
+    private void Update()
+    {
+        if(ranTutorial == false)
+        {
+            TriggerDialogue();
+            ranTutorial = true;
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
