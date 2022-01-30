79f7fe5719f22651268587481ca183f5afbf9ecb
Brayden Coyer <coyerbrayden@gmail.com>
2022-01-30 18:25:50 -0500

    load/save

--- Delivery to Another World/Assets/Scripts/SaveScripts/NewGameSaveOverite.cs
+++ Delivery to Another World/Assets/Scripts/SaveScripts/NewGameSaveOverite.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class SaveIteraction : MonoBehaviour
+public class NewGameSaveOverite : MonoBehaviour
 {
     public GameObject saveDataObj;
     public GameObject playerObj;
