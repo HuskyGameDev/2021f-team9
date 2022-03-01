fd704c24f1e7636548635b02968b23b4b235b9b9
n-wiszowaty <43485233+n-wiszowaty@users.noreply.github.com>
2022-02-28 18:25:26 -0600

    Tutorial Script Done

--- Delivery to Another World/Assets/Scripts/TutorialNPCInteractionScript.cs
+++ Delivery to Another World/Assets/Scripts/TutorialNPCInteractionScript.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TutorialNPCInteractionScript : MonoBehaviour
 {
     private bool rotate = false;
+
+    public Text myText;
+    public string script;
 
     private StreamReader reader;
     private StreamReader alternate;
@@ -14,9 +18,15 @@ public class TutorialNPCInteractionScript : MonoBehaviour
     private Rotation r;
     private bool move;
 
+    private bool talking = false;
+
     private bool intro = true;
     private bool walkedOver = false;
     private bool hasTurned = false;
+    private bool hasTurnedBack = false;
+    private bool done = false;
+
+    private int[] iWillFightUnity = { 2, 3, 3, 3, 4};
 
     // Start is called before the first frame updates
     void Start()
@@ -35,42 +45,127 @@ public class TutorialNPCInteractionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(disappear());
+        }
+
         if (intro)
         {
-            
-            TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Hey welcome to the game. Press continue so I can keep talking", "Now, use WASD to walk over to me" }));
-            intro = false;
+            if (!talking)
+            {
+                talking = true;
+                TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Hey welcome to the game. Press E so I can keep talking", "Now, use WASD to walk over to me" }));
+                //intro = false;
+                iWillFightUnity[0]--;
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                FindObjectOfType<DialogueManager>().DisplayNextScentence();
+                if (iWillFightUnity[0] <= 0)
+                {
+                    intro = false;
+                    talking = false;
+                }
+                iWillFightUnity[0]--;
+            }
         }
 
         if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 1.0f)
         {
-            if (Input.GetKeyDown(KeyCode.E) && !walkedOver)
+            if (Input.GetKeyDown(KeyCode.E) && !walkedOver && !hasTurned)
             {
                 if (!rotate)
                 {
-                    intro = false;
-                    walkedOver = true;
-                    TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Nice job walking over to me", "You're gonna be great at this", "How about you try pressing R?" }));
+                    if (!talking)
+                    {
+                        talking = true;
+                        TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Nice job walking over to me", "You're gonna be great at this", "How about you try pressing R?" }));
+                        iWillFightUnity[1]--;
+                    }
+                    else
+                    {
+                        FindObjectOfType<DialogueManager>().DisplayNextScentence();
+                        if (iWillFightUnity[1] <= 0)
+                        {
+                            walkedOver = true;
+                            talking = false;
+                        }
+                        iWillFightUnity[1]--;
+                    }
                 }
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+        if (!intro && !hasTurned && walkedOver)
         {
-            if (!intro && !hasTurned && walkedOver)
+            if (!talking && Input.GetKeyDown(KeyCode.R))
             {
-                walkedOver = false;
-                hasTurned = true;
+                talking = true;
                 TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Whoa, that wasn't supposed to happen", "Ethan, did you die? Where are you?", "You were supposed to be the chosen one..." }));
+                iWillFightUnity[2]--;
             }
-            else if (!intro && hasTurned && !walkedOver)
+            else if (Input.GetKeyDown(KeyCode.E) && talking)
             {
-                hasTurned = false;
-                TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Oh!", "You're back!", "Want to try sprinting? Press _" }));
+                FindObjectOfType<DialogueManager>().DisplayNextScentence();
+                if (iWillFightUnity[2] <= 0)
+                {
+                    walkedOver = false;
+                    hasTurned = true;
+                    talking = false;
+                }
+                iWillFightUnity[2]--;
             }
-            StartCoroutine(disappear());
+        }
+        else if (hasTurned && !walkedOver && !hasTurnedBack)
+        {
+            if (!talking && Input.GetKeyDown(KeyCode.R))
+            {
+                talking = true;
+                TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Oh!", "You're back!", "Want to try sprinting? Press shift and moving around" }));
+                iWillFightUnity[3]--;
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && talking)
+            {
+                FindObjectOfType<DialogueManager>().DisplayNextScentence();
+
+                if (iWillFightUnity[3] <= 0)
+                {
+                    hasTurned = false;
+                    hasTurnedBack = true;
+                    talking = false;
+                }
+                iWillFightUnity[3]--;
+            }
         }
 
+        if (hasTurnedBack && !done)
+        {
+            if (!talking && (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)))
+            {
+                talking = true;
+                TriggerDialogue(new Dialogue("Sneaky Thief", new string[] { "Nice Job Running!", "I bet when you ... turn invisible...? You'd be able to sprint past people and they would never know!", "Well that's all I can teach you.", "Have fun at the theives guild!" }));
+                iWillFightUnity[4]--;
+            }
+            else if (talking && Input.GetKeyDown(KeyCode.E))
+            {
+                FindObjectOfType<DialogueManager>().DisplayNextScentence();
+                if (iWillFightUnity[4] <= 0)
+                {
+                    hasTurnedBack = false;
+                    done = true;
+                    talking = false;
+                }
+                iWillFightUnity[4]--;
+            }
+        }
+
+        if(done)
+        {
+            //What happens when the tutorial is complete
+            SceneManager.LoadScene("Hub");
+        }
         
 
     }
