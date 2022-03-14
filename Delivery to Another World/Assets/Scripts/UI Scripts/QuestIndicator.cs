aafafa80a35085cb9adfb48f7a9118976cb0cc7a
Brandon Hosang <brandonhosang@yahoo.com>
2022-03-14 17:27:27 -0400

    Updated lighting in hub and tutorial worlds. Also made quest indicator bob up and down.

--- Delivery to Another World/Assets/Scripts/UI Scripts/QuestIndicator.cs
+++ Delivery to Another World/Assets/Scripts/UI Scripts/QuestIndicator.cs
@@ -8,6 +8,15 @@ public class QuestIndicator : MonoBehaviour
     public GameObject questButton1;
     public GameObject questButton2;
 
+    private bool direction;
+    private float startTime;
+
+    private void Start()
+    {
+        direction = false;
+        startTime = Time.time;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -18,6 +27,31 @@ public class QuestIndicator : MonoBehaviour
         else
         {
             GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float t = (Time.time - startTime) / .8f;
+
+        if (!direction)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, Mathf.SmoothStep(0.71f, 1f, t), transform.localPosition.z);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, Mathf.SmoothStep(1f, 0.71f, t), transform.localPosition.z);
+        }
+
+        if (transform.localPosition.y == 1f)
+        {
+            direction = true;
+            startTime = Time.time;
+        }
+        else if (transform.localPosition.y == 0.71f)
+        {
+            direction = false;
+            startTime = Time.time;
         }
     }
 }
