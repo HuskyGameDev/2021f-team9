a406259c150af7c89d1c7c0e4118ddd39c17c71d
n-wiszowaty <43485233+n-wiszowaty@users.noreply.github.com>
2022-04-11 19:41:10 -0500

    I was told to make changes

--- Delivery to Another World/Assets/Scripts/BossRoomButtonInteraction.cs
+++ Delivery to Another World/Assets/Scripts/BossRoomButtonInteraction.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossButtonInteraction : MonoBehaviour
+public class BossRoomButtonInteraction : MonoBehaviour
 {
 
-    public GameObject player;
+    private GameObject player = GameObject.FindGameObjectWithTag("Player");
     public BossRoomButtons bossManager;
 
     // Update is called once per frame
