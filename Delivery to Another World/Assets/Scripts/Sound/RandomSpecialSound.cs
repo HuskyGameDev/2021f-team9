4caf45be62165cc83e8bec757cf9d10dda58fc71
Ethan Wright <ethanw@mtu.edu>
2022-04-18 20:30:32 -0400

    Changed Frequency of Scary Sound

--- Delivery to Another World/Assets/Scripts/Sound/RandomSpecialSound.cs
+++ Delivery to Another World/Assets/Scripts/Sound/RandomSpecialSound.cs
@@ -26,7 +26,7 @@ public class RandomSpecialSound : MonoBehaviour
     IEnumerator CountdownXD()
     {
         wait = true;
-        randomNum = Random.Range(38f, 200f);
+        randomNum = Random.Range(500f, 1000f);
         yield return new WaitForSeconds(randomNum);
         GetComponent<AudioSource>().Play();
         wait = false;
